011be7c25f1e31502ca63e26c63fc238559bb77a
MOUHOUS Mathya <mathya.mouhous@se.univ-bejaia.dz>
2026-03-14 18:32:05 +0100

    JavaScript language test

---
+++ langue.js
@@ -0,0 +1,6 @@
+let lang=document.querySelector('#lang')
+lang.addEventListener("click", ()=>{
+    document.querySelector('h1').textContent="Salut! Mathya MOUHOUS ici"
+    document.querySelector('h2').textContent="Développeuse en génie informatique"
+    document.querySelector('p').innerHTML="Je suis passionnée par le développement logiciel et les technologies web.<br>Toujours impatiente d’apprendre de nouvelles choses chaque jour."
+})
